5fb53c970a8c6fd2ed04cc25dd17e222197e2b1a
[stepsen89] <stephanie.senoner@gmail.com>
2019-10-09 11:33:43 +0100

    added loading screen

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
+$(window).load(function() {
+  // Animate loader off screen
+  $(".se-pre-con").fadeOut("slow");;
+});
+
 function navBar() {
   var x = document.getElementById("header__nav");
   if (x.className === "header__nav") {
